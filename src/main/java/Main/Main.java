0b5ff668025e58b86b72031ada4ed9a677b3a569
mohammed <m.ahmednwq@gmail.com>
2024-10-17 09:52:54 -0400

    Add main folder

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,11 +1,10 @@
-package example;
+package Main;
 
-public class HelloWorld {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
 	}
 
 }
